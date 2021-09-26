426254db381d8978252db59475efdf51e3966377
simplysamiul <simplysamiul@gmail.com>
2021-09-26 11:57:51 +0600

    added bike price and selected bike fuunctionality code

--- src/Component/Bikes/Bikes.js
+++ src/Component/Bikes/Bikes.js
@@ -3,12 +3,13 @@ import Rating from 'react-rating';
 import './Bikes.css';
 
 const Bikes = (props) => {
+    // console.log(props);
     const {model_name, company_name, bike_img, bike_price, discount, rating} = props.bike;
     return (
         <div className="bike-card">
             <img src={bike_img} alt="" />
             <div className="card-text">
-            <h4>{model_name}</h4>
+            <h3>{model_name}</h3>
             <h5>Company : {company_name}</h5>
             <h5>Price : <span className="tk-symbol">&#2547;</span> {bike_price} /-</h5>
             <p>Discount : {discount}</p>
@@ -21,7 +22,7 @@ const Bikes = (props) => {
             />
            </div>
             <div className="buy-button">
-            <button> <span className="buy-icon"><i class="fas fa-shopping-cart"></i></span> buy</button>
+            <button onClick={()=>props.addedSelectedBikes(props.bike)}> <span className="buy-icon"><i className="fas fa-shopping-cart"></i></span> buy</button>
             </div>
             </div>
         </div>
--- src/Component/Garage/Garage.js
+++ src/Component/Garage/Garage.js
@@ -4,21 +4,33 @@ import './Garage.css';
 
 const Garage = () => {
     const [bikes, setBikes] = useState([]);
+    const [selectedBike, setSeletedBike] = useState([]);
     useEffect(()=>{
         fetch('bikes-data.JSON')
         .then(res => res.json())
         .then(data => setBikes(data))
-    },[])
+    },[]);
+    const addedSelectedBikes = eachBike =>{
+        const bike = [...selectedBike, eachBike];
+        setSeletedBike(bike);
+    };
+    let bikePrice  = 0;
+    for(const bike of selectedBike){
+        bikePrice = bikePrice + bike.bike_price;
+    }
     return (
         <div className="garage-area">
             <div className="selected-bikes">
-                <h3>Selected Bikes : 0</h3>
-                <h3>bike Price : </h3>
+                <h3>Selected Bikes : {selectedBike.length}</h3>
+                <h3>bike Price : {bikePrice}</h3>
             </div>
             <div className="bikes-container">
             <div className="bikes-list">
                 {
-                    bikes.map(bike => <Bikes bike={bike} key={bike.id} />)
+                    bikes.map(bike => <Bikes 
+                    bike={bike} key={bike.id} 
+                    addedSelectedBikes = {addedSelectedBikes}
+                    />)
                 }
             </div>
             </div>
